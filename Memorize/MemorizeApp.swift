899b5cec9da8930658a7ef66fc94526f17e637bb
Nikolai Efseaff <nikolaiefseaff@gmail.com>
2020-11-13 15:12:12 -0500

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Nicholas Efseaff on 11/13/20.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
